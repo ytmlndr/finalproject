ca141b20040ca3b84b39d10fbc6b5b080f91db75
ytmlndr <ytmlndr@gmail.com>
2015-03-20 12:59:27 +0200

    heroku R10 error code - fix

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 /**
  * Created by yotam on 3/20/2015.
  */
+var dev = 'dev';
 
 var express = require('express');
 var http = require('http');
@@ -8,15 +9,23 @@ var socketio = require('socket.io');
 var app = express();
 var server = http.Server(app);
 var io = socketio(server);
+var port;
 
 app.get('/', function (req, res) {
     res.end('hello');
-})
+});
 
 io.on('connection', function(socket){
    console.log('a user connected');
 });
 
-server.listen(3000, function(){
-   console.log('listening on port 3000');
+if('dev' == dev) {
+    port = 3000;
+} else {
+    port = process.env.port;
+}
+
+server.listen(port, function(){
+    console.log('listening on port ' + port);
 });
+
